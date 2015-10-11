05a433941be61be8de326415ab8ec331da6b455e
zohooo <zohooo@yeah.net>
2015-10-11 22:44:55 +0800

    normalize full path with wxFileName

--- source/main.lua
+++ source/main.lua
@@ -2,21 +2,25 @@
 package.path = package.path .. ";?.lua;?/?.lua;"
 package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;"
 
+require("wx")
+
 local sep = package.config:sub(1,1) -- path separator
 local maindir, mainlua = "source", "main.lua"
-local tail, length = sep..maindir..sep..mainlua, #maindir + #mainlua + 2
+local tail = maindir..sep..mainlua
 
-mainpath = "."
+mainpath = arg and arg[0]
+    or debug and debug.getinfo(1, "S").source:sub(2)
+    or tail -- assume current working dir is the program dir
 
-if arg[0]:sub(-length) == tail then
-    mainpath = arg[0]:sub(1, -length-1)
-elseif debug then
-    local s = debug.getinfo(1, "S").source
-    if s:sub(-length) == tail and s:sub(1,1) == "@" then
-        mainpath = s:sub(2, -length-1)
-    end
+local f = wx.wxFileName(mainpath)
+if f:Normalize() then
+    mainpath = f:GetFullPath():sub(1, -#tail-2)
+else
+    wx.wxMessageBox("Error in Normalizing Main Path!",
+                    "Error Message", wx.wxOK + wx.wxCENTRE)
+    return
 end
 
 source = mainpath .. sep .. maindir
 
-if not wx then dofile(source .. sep .. "editor.lua") end
+dofile(source .. sep .. "editor.lua")
