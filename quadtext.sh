4a850f88ebf345b6d5d2da0438530e6d3ae3103e
zohooo <zohooo@yeah.net>
2015-10-25 16:25:01 +0800

    add resource files for macosx

--- quadtext.sh
+++ quadtext.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+## this file is modified from zbstudio.sh of ZeroBrane Studio
+
 # see http://stackoverflow.com/a/246128
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# only for macosx
-export DYLD_LIBRARY_PATH="$DIR/binary:${DYLD_LIBRARY_PATH}"
-
-(cd "$DIR"; $DIR/binary/lua $DIR/source/main.lua "$@") &
+if [[ $(uname) == 'Darwin' ]]; then
+    export MAIN_DIR
+    (cd "$MAIN_DIR"; open build/macosx/QuadText.app --args "$@")
+else
+    (cd "$MAIN_DIR"; $MAIN_DIR/binary/lua $MAIN_DIR/source/main.lua "$@") &
+fi
