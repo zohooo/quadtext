ecb52edbbdf69e4633860736d94a089f1f50bf40
zohooo <zohooo@yeah.net>
2015-10-30 09:36:43 +0800

    move codes for creating editor to editor.lua

--- source/editor/gui.lua
+++ source/editor/gui.lua
@@ -303,146 +303,16 @@ function SetDocumentModified(id, modified)
     notebook:SetPageText(openDocuments[id].index, pageText)
 end
 
+dofile(source .. sep .. "editor" .. sep .. "editor.lua")
+
 -- ----------------------------------------------------------------------------
 -- Create an editor and add it to the notebook
 function CreateEditor(name)
-    local editor = wxstc.wxStyledTextCtrl(notebook, editorID,
+    local editor = app:CreateEditor(notebook, editorID,
                                           wx.wxDefaultPosition, wx.wxDefaultSize,
                                           wx.wxSUNKEN_BORDER)
 
     editorID = editorID + 1 -- increment so they're always unique
-
-    editor:SetBufferedDraw(true)
-    editor:StyleClearAll()
-
-    editor:SetFont(font)
-    editor:StyleSetFont(wxstc.wxSTC_STYLE_DEFAULT, font)
-    for i = 0, 32 do
-        editor:StyleSetFont(i, font)
-    end
-
-    editor:SetUseTabs(false)
-    editor:SetTabWidth(4)
-    editor:SetIndent(4)
-    editor:SetIndentationGuides(true)
-
-    local wrapmode = app.setting.editor.wrapmode or 1
-    editor:SetWrapMode(tonumber(wrapmode))
-
-    editor:SetVisiblePolicy(wxstc.wxSTC_VISIBLE_SLOP, 3)
-    --editor:SetXCaretPolicy(wxstc.wxSTC_CARET_SLOP, 10)
-    --editor:SetYCaretPolicy(wxstc.wxSTC_CARET_SLOP, 3)
-
-    editor:SetCaretLineVisible(true)
-    editor:SetCaretLineBackground(wx.wxColour(244, 244, 222))
-
-    editor:SetMarginWidth(0, editor:TextWidth(32, "99999_")) -- line # margin
-
-    editor:SetMarginWidth(1, 16) -- marker margin
-    editor:SetMarginType(1, wxstc.wxSTC_MARGIN_SYMBOL)
-    editor:SetMarginSensitive(1, true)
-
-    editor:MarkerDefine(CURRENT_LINE_MARKER, wxstc.wxSTC_MARK_ARROW,     wx.wxBLACK, wx.wxGREEN)
-
-    editor:SetMarginWidth(2, 16) -- fold margin
-    editor:SetMarginType(2, wxstc.wxSTC_MARGIN_SYMBOL)
-    editor:SetMarginMask(2, wxstc.wxSTC_MASK_FOLDERS)
-    editor:SetMarginSensitive(2, true)
-
-    editor:SetFoldFlags(wxstc.wxSTC_FOLDFLAG_LINEBEFORE_CONTRACTED +
-                        wxstc.wxSTC_FOLDFLAG_LINEAFTER_CONTRACTED)
-
-    editor:SetProperty("fold", "1")
-    editor:SetProperty("fold.compact", "1")
-    editor:SetProperty("fold.comment", "1")
-
-    local grey = wx.wxColour(128, 128, 128)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDEROPEN,    wxstc.wxSTC_MARK_BOXMINUS, wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDER,        wxstc.wxSTC_MARK_BOXPLUS,  wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDERSUB,     wxstc.wxSTC_MARK_VLINE,    wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDERTAIL,    wxstc.wxSTC_MARK_LCORNER,  wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDEREND,     wxstc.wxSTC_MARK_BOXPLUSCONNECTED,  wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDEROPENMID, wxstc.wxSTC_MARK_BOXMINUSCONNECTED, wx.wxWHITE, grey)
-    editor:MarkerDefine(wxstc.wxSTC_MARKNUM_FOLDERMIDTAIL, wxstc.wxSTC_MARK_TCORNER,  wx.wxWHITE, grey)
-    grey:delete()
-
-    editor:Connect(wxstc.wxEVT_STC_MARGINCLICK,
-            function (event)
-                local line = editor:LineFromPosition(event:GetPosition())
-                local margin = event:GetMargin()
-                if margin == 2 then
-                    if wx.wxGetKeyState(wx.WXK_SHIFT) and wx.wxGetKeyState(wx.WXK_CONTROL) then
-                        FoldSome()
-                    else
-                        local level = editor:GetFoldLevel(line)
-                        if HasBit(level, wxstc.wxSTC_FOLDLEVELHEADERFLAG) then
-                            editor:ToggleFold(line)
-                        end
-                    end
-                end
-            end)
-
-    editor:Connect(wxstc.wxEVT_STC_CHARADDED,
-            function (event)
-                -- auto-indent
-                local ch = event:GetKey()
-                if (ch == char_CR) or (ch == char_LF) then
-                    local pos = editor:GetCurrentPos()
-                    local line = editor:LineFromPosition(pos)
-
-                    if (line > 0) and (editor:LineLength(line) == 0) then
-                        local indent = editor:GetLineIndentation(line - 1)
-                        if indent > 0 then
-                            editor:SetLineIndentation(line, indent)
-                            editor:GotoPos(pos + indent)
-                        end
-                    end
-                elseif autoCompleteEnable then -- code completion prompt
-                    local pos = editor:GetCurrentPos()
-                    local start_pos = editor:WordStartPosition(pos, true)
-                    -- must have "wx.X" otherwise too many items
-                    if (pos - start_pos > 0) and (start_pos > 2) then
-                        local range = editor:GetTextRange(start_pos-3, start_pos)
-                        if range == "wx." then
-                            local commandEvent = wx.wxCommandEvent(wx.wxEVT_COMMAND_MENU_SELECTED,
-                                                                   ID_AUTOCOMPLETE)
-                            wx.wxPostEvent(frame, commandEvent)
-                        end
-                    end
-                end
-            end)
-
-    editor:Connect(wxstc.wxEVT_STC_USERLISTSELECTION,
-            function (event)
-                local pos = editor:GetCurrentPos()
-                local start_pos = editor:WordStartPosition(pos, true)
-                editor:SetSelection(start_pos, pos)
-                editor:ReplaceSelection(event:GetText())
-            end)
-
-    editor:Connect(wxstc.wxEVT_STC_SAVEPOINTREACHED,
-            function (event)
-                SetDocumentModified(editor:GetId(), false)
-            end)
-
-    editor:Connect(wxstc.wxEVT_STC_SAVEPOINTLEFT,
-            function (event)
-                SetDocumentModified(editor:GetId(), true)
-            end)
-
-    editor:Connect(wxstc.wxEVT_STC_UPDATEUI,
-            function (event)
-                UpdateStatusText(editor)
-            end)
-
-    editor:Connect(wx.wxEVT_SET_FOCUS,
-            function (event)
-                event:Skip()
-                if in_evt_focus or exitingProgram then return end
-                in_evt_focus = true
-                IsFileAlteredOnDisk(editor)
-                in_evt_focus = false
-            end)
 
     if notebook:AddPage(editor, name, true) then
         local id            = editor:GetId()
