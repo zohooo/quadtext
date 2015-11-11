904eb9b141ecb503460a5d7ce4c33b79b9416f1d
zohooo <zohooo@yeah.net>
2015-11-11 19:38:23 +0800

    default to Consolas font

--- source/setting/setting-editor.lua
+++ source/setting/setting-editor.lua
@@ -2,7 +2,7 @@
 app.setting.editor = {
     theme = "light",
     encoding = {"UTF-8", "System"},
-    fontname = "Courier New",
+    fontname = "Consolas",
     fontsize = 11,
     wrapmode = 2,
 }
