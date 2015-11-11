d4ab6693346a9b54bb44dc8cfa3824171fe9c618
zohooo <zohooo@yeah.net>
2015-11-11 10:46:11 +0800

    add lua-iconv to windows build script

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -34,6 +34,14 @@ WXLUA_BASENAME="wxLua-2.8.12.3-src"
 WXLUA_FILENAME="$WXLUA_BASENAME.tar.gz"
 WXLUA_URL="$SOURCEFORGE/wxlua/wxlua/2.8.12.3/$WXLUA_FILENAME"
 
+ICONV_BASENAME="libiconv-1.14"
+ICONV_FILENAME="$ICONV_BASENAME.tar.gz"
+ICONV_URL="http://ftp.gnu.org/pub/gnu/libiconv/$ICONV_FILENAME"
+
+LUAICONV_BASENAME="lua-iconv-master"
+LUAICONV_FILENAME="$LUAICONV_BASENAME.tar.gz"
+LUAICONV_URL="https://github.com/ittner/lua-iconv/archive/master.tar.gz"
+
 # exit if the command line is empty
 if [ $# -eq 0 ]; then
     echo "Usage: $0 LIBRARY..."
@@ -56,6 +64,10 @@ for ARG in "$@"; do
     wxlua)
         BUILD_WXLUA=true
         ;;
+    luaiconv)
+        BUILD_ICONV=true
+        BUILD_LUAICONV=true
+        ;;
     debug)
         WXLUASTRIP=""
         WXWIDGETSDEBUG="--enable-debug=max --enable-debug_gdb"
@@ -67,6 +79,8 @@ for ARG in "$@"; do
         BUILD_WXWIDGETS=true
         BUILD_LUA=true
         BUILD_WXLUA=true
+        BUILD_ICONV=true
+        BUILD_LUAICONV=true
         ;;
     *)
         echo "Error: invalid argument $ARG"
@@ -98,7 +112,7 @@ function prepare_source {
     # $1 = basename, $2 = filename, $3 = url
     if [ ! -d $1 ]; then
         if [ ! -f $2 ]; then
-            wget -c "$3" -O "$2" || { echo "Error: failed to download $2"; exit 1; }
+            wget --no-check-certificate -c "$3" -O "$2" || { echo "Error: failed to download $2"; exit 1; }
         fi
         tar -xzf "$2"
     fi
@@ -163,6 +177,23 @@ if [ $BUILD_WXLUA ]; then
     cd ..
 fi
 
+if [ $BUILD_ICONV ]; then
+    prepare_source $ICONV_BASENAME $ICONV_FILENAME $ICONV_URL
+    cd "$ICONV_BASENAME"
+    ./configure --prefix="$INSTALL_DIR" --disable-shared --enable-static --disable-nls
+    make $MAKEFLAGS || { echo "Error: failed to build iconv"; exit 1; }
+    make install
+fi
+
+if [ $BUILD_LUAICONV ]; then
+    prepare_source $LUAICONV_BASENAME $LUAICONV_FILENAME $LUAICONV_URL
+    cd "$LUAICONV_BASENAME"
+    gcc -c -o iconv.lo $BUILD_FLAGS luaiconv.c
+    gcc -o iconv.dll $BUILD_FLAGS iconv.lo $INSTALL_DIR/lib/lua$LUAS.dll -liconv
+    cp iconv.dll "$INSTALL_DIR/bin"
+    [ -f "$INSTALL_DIR/bin/iconv.dll" ] || { echo "Error: iconv.dll isn't found"; exit 1; }
+fi
+
 # now copy the compiled dependencies to binary directory
 if [ ! -d $BIN_DIR ]; then
     mkdir -p "$BIN_DIR" || { echo "Error: cannot create directory $BIN_DIR"; exit 1; }
@@ -170,6 +201,7 @@ fi
 
 [ $BUILD_LUA ] && cp "$INSTALL_DIR/bin/lua.exe" "$INSTALL_DIR/lib/lua$LUAS.dll" "$BIN_DIR"
 [ $BUILD_WXLUA ] && cp "$INSTALL_DIR/bin/libwx.dll" "$BIN_DIR/wx.dll"
+[ $BUILD_LUAICONV ] && cp "$INSTALL_DIR/bin/iconv.dll" "$BIN_DIR/iconv.dll"
 
 # To build lua5.2.dll proxy:
 # (1) get mkforwardlib-gcc-52.lua from http://lua-users.org/wiki/LuaProxyDllThree
